8c31775b3af876e9c1509b1dc5920f71e2be5d52
fzyzcjy <ch271828n@outlook.com>
2023-01-23 16:04:01 +0800

    minor change scripts to use minif2f unconditionally

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# NOTE(fzyzcjy) originally `setup_miniF2F.sh`
+
 # run from project root
 
+rm -rf leanpkg.path
+rm -rf _target
+
+# cp leanpkg_miniF2F.toml leanpkg.toml # NOTE (fzyzcjy) already copied it to leanpkg.toml
 leanpkg configure
 bash ./_target/deps/mathlib/scripts/mk_all.sh
+
+mv ./_target/deps/mathlib/src/all.lean ./_target/deps/minif2f/lean/src/all.lean
+echo "import test" >> ./_target/deps/minif2f/lean/src/all.lean
+echo "import valid" >> ./_target/deps/minif2f/lean/src/all.lean
+
 leanproject get-mathlib-cache
 leanpkg build
